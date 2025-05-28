5feec58c08b6a242c86bd543e6cfc5444b7f995f
Ghost-Eyes22 <isaiah.torres@stu.vertexacademies.org>
2025-05-28 18:13:20 +0000

    finished 3.2.1.

--- 321_lists_Torres.py
+++ 321_lists_Torres.py
@@ -1,1 +1,14 @@
-print("Hello world i'm SpiderMan!")
+# a list of menu items
+food = ["Burger", "Shake", "Fries", "Chicken"]
+
+print(food[0], "is located at index 0.")
+print(food[1], "is located at index 0.")
+print(food[2], "is located at index 0.")
+print(food[3], "is located at index 0.")
+print(food[-1], "is located at index -1")
+
+if("chicken" in food):
+    print("You can have a chicken!")
+else:
+    print("We don't have chicken's")
+    
